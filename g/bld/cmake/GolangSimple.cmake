8b9199903f1224150f07dd8b5e401daddf9d42ef
Matteo Joliveau <m.joliveau@koor.it>
2017-08-24 12:27:01 +0200

    Migrated to integrate go_cmake

---
+++ g/bld/cmake/GolangSimple.cmake
@@ -0,0 +1,36 @@
+
+set(GOPATH "${CMAKE_CURRENT_BINARY_DIR}/go")
+file(MAKE_DIRECTORY ${GOPATH})
+
+function(GO_GET TARG)
+  add_custom_target(${TARG} env GOPATH=${GOPATH} go get ${ARGN})
+endfunction(GO_GET)
+
+function(GO_COPY MODULE_NAME MODULE_SRC)
+    get_filename_component(MODULE_SRC_ABS "../../${MODULE_SRC}" ABSOLUTE)
+
+    message(STATUS "Copying Local Module ${MODULE_SRC_ABS} to ${GOPATH}/src/${MODULE_SRC}")
+    add_custom_target(${MODULE_NAME}
+                COMMAND ${CMAKE_COMMAND} -E copy_directory
+                ${MODULE_SRC_ABS} ${GOPATH}/src/${MODULE_SRC})
+
+endfunction(GO_COPY)
+
+function(ADD_GO_INSTALLABLE_PROGRAM NAME MAIN_SRC)
+  get_filename_component(MAIN_SRC_ABS ${MAIN_SRC} ABSOLUTE)
+  add_custom_target(${NAME})
+  add_custom_command(TARGET ${NAME}
+                    COMMAND env GOPATH=${GOPATH} go build 
+                    -o "${CMAKE_CURRENT_BINARY_DIR}/${NAME}"
+                    ${CMAKE_GO_FLAGS} ${MAIN_SRC}
+                    WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
+                    DEPENDS ${MAIN_SRC_ABS})
+  foreach(DEP ${ARGN})
+    add_dependencies(${NAME} ${DEP})
+  endforeach()
+  
+  add_custom_target(${NAME}_all ALL DEPENDS ${NAME})
+  install(PROGRAMS ${CMAKE_CURRENT_BINARY_DIR}/${NAME} DESTINATION bin)
+endfunction(ADD_GO_INSTALLABLE_PROGRAM)
+
+
