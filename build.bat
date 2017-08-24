075d1a3e1b1755ea354bea5882bd426545561b91
Matteo Joliveau <m.joliveau@koor.it>
2017-08-24 15:34:10 +0200

    Build scripts

--- build.bat
+++ build.bat
@@ -1,2 +1,40 @@
-go get ./...
-go build -o qk.exe
+@echo off
+
+SET APP_NAME=Quicken Project Generator
+SET APP_PATH=%~dp0
+
+cd /d %~dp0
+ECHO
+SET NAMESPACE=github.com/matteojoliveau/quicken
+SET MODULES=cmd modules recipe utils
+SET WORKDIR=%APP_PATH%build\go\src
+ECHO %WORKDIR%
+
+if not exist %WORKDIR% mkdir "%WORKDIR%\%NAMESPACE%"
+
+setlocal ENABLEDELAYEDEXPANSION
+
+cd %APP_PATH%
+
+SET GOPATH=%APP_PATH%build\go
+
+goto :run
+
+:run
+ECHO --------------------------------------------------------------
+ECHO Copying project modules...
+For %%a in (%MODULES%) do robocopy  %%a "%WORKDIR%\%NAMESPACE%\%%a" /MIR
+ rem xcopy /s /y %%a "%WORKDIR%\%NAMESPACE%\%%a"
+
+IF "%1" NEQ "-d" (
+    ECHO Getting dependencies...
+    go get ./...
+) ELSE ECHO DEBUG MODE: Skipping dependencies download
+ECHO --------------------------------------------------------------
+echo Building project...
+go build -o build/qk.exe
+ECHO Done!
+goto :end
+:end
+endlocal
+REM pause
--- build.sh
+++ build.sh
@@ -1,5 +1,31 @@
 #!/usr/bin/env bash
 
-go get ./...
-go build -o qk
-chmod +x qk
+NAMESPACE=github.com/matteojoliveau/quicken
+MODULES="cmd/ modules/ recipe/ utils/"
+WORKDIR=build/go/src
+sep="-----------------------------------"
+echo ${sep}
+echo ${sep}
+echo "Building Quicken!"
+echo ${sep}
+echo ${sep}
+
+printf "Creating Workdir...\n\n"
+
+mkdir -p ${WORKDIR}/${NAMESPACE}
+GOPATH=$(pwd)/build/go
+printf "Using GOPATH: ${GOPATH}\n"
+printf "Copying project modules...\n\n"
+cp -r ${MODULES} ${WORKDIR}/${NAMESPACE}
+if [ "$1" != "-d" ]; then #debug mode
+    printf "Getting dependencies...\n\n"
+    env GOPATH=${GOPATH} go get ./...
+else
+    printf "DEBUG MODE: Skipping dependencies download\n"
+fi
+wait $!
+printf "Building project...\n\n"
+env GOPATH=${GOPATH} go build -o build/qk
+wait $!
+printf "Done!"
+chmod +x build/
