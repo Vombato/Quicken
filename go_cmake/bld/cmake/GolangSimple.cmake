f23d0e2f5ef59ecda83bf74908d0772c01074005
Matteo Joliveau <m.joliveau@koor.it>
2017-08-24 13:24:17 +0200

    Namespaced

--- go_cmake/bld/cmake/GolangSimple.cmake
+++ go_cmake/bld/cmake/GolangSimple.cmake
@@ -7,12 +7,12 @@ function(GO_GET TARG)
 endfunction(GO_GET)
 
 function(GO_COPY MODULE_NAME MODULE_SRC)
-    get_filename_component(MODULE_SRC_ABS "../../${MODULE_SRC}" ABSOLUTE)
+    get_filename_component(MODULE_SRC_ABS "${MODULE_SRC}" ABSOLUTE)
 
-    message(STATUS "Copying Local Module ${MODULE_SRC_ABS} to ${GOPATH}/src/${MODULE_SRC}")
+    message(STATUS "Copying Local Module ${MODULE_SRC_ABS} to ${GOPATH}/src/${GO_NAMESPACE}/${MODULE_SRC}")
     add_custom_target(${MODULE_NAME}
                 COMMAND ${CMAKE_COMMAND} -E copy_directory
-                ${MODULE_SRC_ABS} ${GOPATH}/src/${MODULE_SRC})
+                ${MODULE_SRC_ABS} ${GOPATH}/src/${GO_NAMESPACE}/${MODULE_SRC})
 
 endfunction(GO_COPY)
 
@@ -21,7 +21,7 @@ function(ADD_GO_INSTALLABLE_PROGRAM NAME MAIN_SRC)
   add_custom_target(${NAME})
   add_custom_command(TARGET ${NAME}
                     COMMAND env GOPATH=${GOPATH} go build 
-                    -o "${CMAKE_CURRENT_BINARY_DIR}/${NAME}"
+                    -o "${CMAKE_CURRENT_BINARY_DIR}/${NAME}$ENV{GOEXE}"
                     ${CMAKE_GO_FLAGS} ${MAIN_SRC}
                     WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
                     DEPENDS ${MAIN_SRC_ABS})
